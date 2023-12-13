d2c57eebf0a0d786d8735f0ab4c47a4f6bc7035c
SeungYeong CHOE <149995855+SY-Cpp@users.noreply.github.com>
2023-12-13 18:08:01 +0900

    Time: 47 ms (48.11%), Space: 56 MB (9.44%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i, j=0;
+
+        for (i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] ==0) 
+            {
+                k--;
+            }
+            if(k < 0 && nums[j++] == 0)
+            {
+                k++;
+            }
+
+        }
+        return i-j;
+    }
+};
